0a0f03c76549dd74d88666c4fe0944d6512c51e0
kokoagb <kokoagb@gmail.com>
2020-03-19 23:46:25 +0100

    add button component

---
+++ client/components/Button/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="button" :class="`button-${variant}`" @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: 'default'
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.button {
+  @apply py-2 px-4 rounded-md shadow-md;
+}
+
+.button.button-default {
+  @apply bg-teal-100;
+}
+
+.button.button-cta {
+  @apply bg-teal-500 text-teal-100;
+}
+
+.button:focus {
+  @apply outline-none shadow-outline;
+}
+
+.button:disabled {
+  opacity: 0.5;
+}
+</style>
