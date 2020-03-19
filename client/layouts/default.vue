edae23afcb5773d3ee1a194a90dc96222191ce53
kokoagb <kokoagb@gmail.com>
2020-03-19 23:48:57 +0100

    update default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <page-header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import PageHeader from '@/components/PageHeader'
+
+export default {
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style lang="postcss">
+body {
+  @apply bg-gray-100;
+}
+</style>
